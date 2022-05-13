9c1010618360a1d87ab992e4e01d0df58bdf5e86
Sherdil01 <105424483+Sherdil01@users.noreply.github.com>
2022-05-13 10:51:06 +0500

    7 masala javobiga o`zgartirish kiritildi
    7 masala o`zgartirildi

--- #7.py
+++ #7.py
@@ -1,19 +1,3 @@
-# #include<iostream>                                                                                                      
-# using namespace std;
-# int main()
-# {
-# long int i=1,s=0,fib=2,r,n; 
-#     cin>>n;
-#     while (i<n)
-# {
-#     r=s+fib; 
-#   s=fib; 
-#   fib=r;
-#     i=i+1;
-# }
-#     cout<<fib;
-#     return 0;
-# }
 N = int(input())
 i=1
 s=0
